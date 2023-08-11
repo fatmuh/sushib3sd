bdcb3837a517319205245ac04c95d9258cd613e9
Fathur Muhammad <fatmuhdev@gmail.com>
2023-08-12 00:00:39 +0700

    Rollback

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Test Git Cpanel</div>
+                                Total Kategori</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kategori }}</div>
                         </div>
                     </div>
